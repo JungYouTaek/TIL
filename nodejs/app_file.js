19203c4c48dcea9506e0139d897c8322828ac3a6
JungYouTaek <jyt9319@gmail.com>
2017-06-12 06:50:27 +0900

    [nodejs]after refactoring

--- nodejs/app_file.js
+++ nodejs/app_file.js
@@ -7,32 +7,42 @@ app.locals.pretty = true;
 app.set('views', './views_file');
 app.set('view engine', 'jade');
 app.get('/topic/new', function(req, res) {
-  res.render('new');
+  fs.readdir('data', function(err, files) {
+    if(err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    res.render('new', {topics:files});
+  });
 });
-
-app.get('/topic', function(req, res){
+app.get(['/topic', '/topic/:id'], function(req, res){
   fs.readdir('data', function(err, files){
     if(err) {
       console.log(err);
       res.status(500).send('Internal Server Error');
     }
-    res.render('view', {topics:files});
+    var id = req.params.id;
+    if(id) {
+      // id 있을 때
+      fs.readFile('data/'+id, 'utf8', function(err, data){
+        if(err) {
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+        }
+        res.render('view', {topics:files, title:id, description:data});
+      });
+    } else {
+      //id 없을 때
+      res.render('view', {topics:files, title:'Welcome', description:'hihi'});
+    }
   });
 });
 app.get('/topic/:id', function(req, res){
-  var id = req.params.id;
   fs.readdir('data', function(err, files){
     if(err) {
       console.log(err);
       res.status(500).send('Internal Server Error');
     }
-    fs.readFile('data/'+id, 'utf8', function(err, data){
-      if(err) {
-        console.log(err);
-        res.status(500).send('Internal Server Error');
-      }
-      res.render('view', {topics:files, title:id, description:data});
-    });
   });
 })
 app.post('/topic', function(req, res){
@@ -44,7 +54,7 @@ app.post('/topic', function(req, res){
     }
     res.sned('Hi, post' + title);
   });
-  res.send('Success!');
+  res.redirect('/topic/'+title);
 })
 app.listen(3000, function() {
   console.log('Connected, 3000 port!');
